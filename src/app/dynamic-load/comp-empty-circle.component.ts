c628b7e694850ac0ab4b04267d9ad60798f62beb
dolphinedaisy <dhwanij2@gmail.com>
2020-07-22 19:57:32 +0530

    input box for tape diagram added

--- src/app/dynamic-load/comp-empty-circle.component.ts
+++ src/app/dynamic-load/comp-empty-circle.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comp-empty-circle',
-  template: `<img class="m-3" height="30" width="30" src="assets/img/mybackyardlab/empty-circle.svg" alt="">`,
+  template: `<img class="mx-3" height="30" width="30" src="assets/img/mybackyardlab/empty-circle.svg" alt="">`,
   styles: [``]
 })
 export class CompEmptyCircleComponent implements OnInit {
--- src/app/dynamic-load/comp-filled-circle.component.ts
+++ src/app/dynamic-load/comp-filled-circle.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comp-filled-circle',
-  template: `<img class="m-3" height="30" width="30" src="assets/img/mybackyardlab/filled-circle.svg" alt="">`,
+  template: `<img class="mx-3" height="30" width="30" src="assets/img/mybackyardlab/filled-circle.svg" alt="">`,
   styles: [``]
 })
 export class CompFilledCircleComponent implements OnInit {
